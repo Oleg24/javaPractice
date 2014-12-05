40fd587689a93c7d809b1718d6e7f6dad8add13b
Oleg <o.yanchinskiy@gmail.com>
2014-12-04 22:31:55 -0800

    Small practice test with conditional

--- youtube.java
+++ youtube.java
@@ -2,8 +2,11 @@
 
 class apples {
   public static void main(String args[]){
+    int age = 12;
 
-    int t = a;
+    if(age > 14){
+      System.printf("you're old");
+    }
     b = t;
     a =b;
   }
